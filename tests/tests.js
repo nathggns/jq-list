2891b1d5e656747c4e7ee151c3c7b3285694a7d6
James Hall <james@snapshotmedia.co.uk>
2013-02-09 17:19:33 +0000

    Test delete

--- tests/tests.js
+++ tests/tests.js
@@ -1,30 +1,11 @@
 
 
-
-// test('Test basic functionality', function() {
-
-// 	// Type in first input
-// 	$('input').eq(0).val('Some text');
-
-// 	ok($('input').eq(0).val() == 'Some text', 'Passed');
-
-// 	// Type in second input
-// 	$('input').eq(1).val('Some more text');
-
-// 	// Add another child to first element
-// 	$('span[data-role=add]').eq(0).click();
-// 	$('input').eq(2).val('This is some more');
-
-// 	ok($('input').eq(2).val() == 'This is some more', 'Check value before add');
-
-// });
-
 test('Test preserving sublist contents', function() {
 
 	// Type in first input
 	$('input').eq(0).val('Some text');
 
-	ok($('input').eq(0).val() == 'Some text', 'Passed');
+	equal($('input').eq(0).val(), 'Some text', 'Passed');
 
 	// Type in second input
 	$('input').eq(1).val('Some more text');
@@ -33,11 +14,21 @@ test('Test preserving sublist contents', function() {
 	$('span[data-role=add]').eq(0).click();
 	$('input').eq(2).val('This is some more');
 
-	ok($('input').eq(2).val() == 'This is some more', 'Check value before add');
+	// Remove that 'This is some more' item
+	$('span[data-role=delete]').eq(1).click();
+
+	// Test to see if its gone by making sure the list length it 1
+	equal($('ul.list > li > ul').length, 1, 'Check list length after delete');
+
+	// Add another child to first element
+	$('span[data-role=add]').eq(0).click();
+	$('input').eq(2).val('This is some more');
+
+	equal($('input').eq(2).val(), 'This is some more', 'Check value before add');
 
 	// Add another child to first element
 	$('ul.list > li > div span[data-role=add]').click();
 
-	ok($('input').eq(2).val() == 'This is some more', 'Check value after add');
+	equal($('input').eq(2).val(), 'This is some more', 'Check value after add');
 
 });
