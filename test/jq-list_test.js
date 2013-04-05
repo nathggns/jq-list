6b176855a2818a347ae844fb9c951978e47f6ae5
Nathaniel Higgins <nat@nath.is>
2013-04-05 12:06:20 +0100

    Write failing tests.

--- test/jq-list_test.js
+++ test/jq-list_test.js
@@ -119,6 +119,48 @@
       ok($new.is('[data-identifier]'), 'identifier attribute added when using element');
       ok($new.is('[data-parent]'), 'parent attribute added when using element');
       ok($new.is('[data-index]'), 'index attribute added when using element');
+    },
+
+    'test deleting items with children': function($element) {
+      var run = $element.list('methodRunner');
+      var first = Math.floor(Math.random() * 10000);
+      var second = Math.floor(Math.random() * 10000);
+      var third = Math.floor(Math.random() * 10000);
+      var fourth = Math.floor(Math.random() * 10000);
+      var fifth = Math.floor(Math.random() * 10000);
+
+      $element.find('> li > input').val(first);
+
+      var sub = $element.find('> li [data-sublist]');
+
+      sub.find('input').eq(0).val(second);
+      sub.find('[data-role=add]').eq(0).click();
+      sub.find('input').eq(1).val(third);
+
+      $element.find('> li > .buttons [data-role=add]').click();
+
+      var li = $element.find('> li:not(:first-child)');
+
+      li.find('input').val(fourth);
+
+      li.find('ul').find('input').val(fifth);
+
+      equal(sub.children().length, 2, 'Children length');
+      equal($element.children().length, 2, 'Element children');
+      equal(li.find('ul').children().length, 1, 'Second children length');
+
+      equal($element.find('> li:first-child > input').val(), first, 'First val');
+      equal($element.find('> li:first-child [data-sublist]').children().eq(0).find('input').val(), second, 'Second val');
+      equal($element.find('> li:first-child [data-sublist]').children().eq(1).find('input').val(), third, 'Third val');
+      equal($element.find('> li:not(:first-child) > input').val(), fourth, 'Fourth val');
+      equal($element.find('> li:not(:first-child) [data-sublist]').children().eq(0).find('input').val(), fifth, 'Fifth val');
+
+      $element.find('> li > .buttons [data-role=delete]').click();
+
+      equal($element.children().length, 1, 'Length after delete');
+      equal($element.find('> li:first-child > input').val(), fourth, 'Fourth val');
+      equal($element.find('> li:first-child [data-sublist]').children().length, 1, 'child length after delete');
+      equal($element.find('> li:first-child [data-sublist]').children().eq(0).find('input').val(), fifth, 'Fifth val after delete');
     }
   };
 
